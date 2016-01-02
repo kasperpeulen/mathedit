7f6e0ba5a50a30bad07ce75026015b1ba210172b
Kasper Peulen <kasperpeulen@gmail.com>
2016-01-02 23:39:30 +0100

    Built web

--- service_worker.js
+++ service_worker.js
@@ -3,16 +3,16 @@
 
     importScripts('sw_toolbox.js');
 
-    global.toolbox.router.get('/mathedit', toolbox.cacheFirst);
-    global.toolbox.router.get('/mathedit/', toolbox.cacheFirst);
+    global.toolbox.router.get('/mathedit', toolbox.fastest);
+    global.toolbox.router.get('/mathedit/', toolbox.fastest);
     global.toolbox.router.get('/mathedit/#/gist/:gist', toolbox.networkFirst);
-    global.toolbox.router.get('/mathedit/fonts/(.*)', toolbox.cacheFirst);
-    global.toolbox.router.get('/mathedit/main.dart.js', toolbox.cacheFirst);
+    global.toolbox.router.get('/mathedit/fonts/(.*)', toolbox.fastest);
+    global.toolbox.router.get('/mathedit/main.dart.js', toolbox.fastest);
 
     // By default, all requests that don't match our custom handler will use the
     // toolbox.networkFirst cache strategy, and their responses will be stored in
     // the default cache.
-    global.toolbox.router.default = global.toolbox.cacheFirst;
+    global.toolbox.router.default = global.toolbox.fastest;
 
 
     // Boilerplate to ensure our service worker takes control of the page as soon
